78d7adb38feb9743f6c6e6fb2dc0def465ac4e51
Noah Rossi <me@noahrossi.com>
2020-04-14 17:17:43 -0400

    load distribution

--- index.js
+++ index.js
@@ -7,16 +7,27 @@ addEventListener('fetch', event => {
  * @param {Request} request
  */
 async function handleRequest(request) {
-  const variant_url = 'https://cfw-takehome.developers.workers.dev/api/variants'
+  const variantUrl = 'https://cfw-takehome.developers.workers.dev/api/variants'
 
   // request variants from API
-  let response = await fetch(variant_url, {})
-  let variants = await response.json()
+  let cfReq = await fetch(variantUrl, {})
+  let variants = await cfReq.json()
 
   // clean up JSON into our array
   variants = variants.variants
 
-  return new Response('Hello worker!', {
-    headers: { 'content-type': 'text/plain' },
+  let page
+
+  // distribute requests 50/50
+  if (Math.random() > 0.5) {
+    let pageReq = await fetch(variants[0], {})
+    page = await pageReq.text()
+  } else {
+    let pageReq = await fetch(variants[1], {})
+    page = await pageReq.text()
+  }
+
+  return new Response(page, {
+    headers: { 'content-type': 'text/HTML' },
   })
 }
